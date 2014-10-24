de4bdf54c67ca8b92022caa4608726fb702030d9
Misha <michail.belyaevq@gmail.com>
2014-10-24 17:24:08 +0400

    Changes to be committed: 	new file:   SecondProject/Assets/Fonts/RobotoCondensed-Regular.ttf 	new file:   SecondProject/Assets/Scripts/onClickStone.cs 	new file:   SecondProject/Assets/SecondProject.unity 	new file:   SecondProject/Assets/Textures/cartoon-stone-hi.png 	new file:   SecondProject/ProjectSettings/AudioManager.asset 	new file:   SecondProject/ProjectSettings/DynamicsManager.asset 	new file:   SecondProject/ProjectSettings/EditorBuildSettings.asset 	new file:   SecondProject/ProjectSettings/EditorSettings.asset 	new file:   SecondProject/ProjectSettings/GraphicsSettings.asset 	new file:   SecondProject/ProjectSettings/InputManager.asset 	new file:   SecondProject/ProjectSettings/NavMeshLayers.asset 	new file:   SecondProject/ProjectSettings/NetworkManager.asset 	new file:   SecondProject/ProjectSettings/Physics2DSettings.asset 	new file:   SecondProject/ProjectSettings/ProjectSettings.asset 	new file:   SecondProject/ProjectSettings/QualitySettings.asset 	new file:   SecondProject/ProjectSettings/TagManager.asset 	new file:   SecondProject/ProjectSettings/TimeManager.asset 	new file:   SecondProject/SecondProject.userprefs

---
+++ SecondProject/Assets/Scripts/onClickStone.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class onClickStone : MonoBehaviour
+{
+	
+		public int count = 0;
+		float _width = 480f;
+		float _height = 854f;
+
+		// Use this for initialization
+		void Start ()
+		{
+				float scaleX = Screen.width / _width;
+				float scaleY = Screen.height / _height;
+
+				GUIText[] texts = FindObjectsOfType (typeof(GUIText)) as GUIText[]; 
+				foreach (GUIText myText in texts) { //find your element of text
+			
+						Vector2 pixOff = myText.pixelOffset; //your pixel offset on screen
+						int origSizeText = myText.fontSize;
+						myText.pixelOffset = new Vector2 (pixOff.x * scaleX, pixOff.y * scaleY); //new position
+						float floatFontSize = origSizeText * scaleX; //new size font in a float
+						myText.fontSize = (int)Mathf.RoundToInt (floatFontSize); // Closest value of fontSize
+			
+				}
+		}
+
+		void Awake ()
+		{
+				Application.targetFrameRate = 60;
+		}
+		// Update is called once per frame
+		void Update ()
+		{
+				// обработка тача и апдейт count
+
+				if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began) {
+						//if (Input.GetMouseButtonDown (0)) {
+						Vector2 pos = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
+						GUIText ThisText = GameObject.Find ("GUI Text").GetComponent<GUIText> ();
+						RaycastHit2D hitInfo = Physics2D.Raycast (Camera.main.ScreenToWorldPoint (pos), Vector2.zero);
+						//ThisText.text = ThisText.fontSize.ToString();
+						if (hitInfo.collider != null) {
+								count++;
+								ThisText.text = "Score:" + count;
+						}
+
+				}
+		}
+		
+		void OnGUI ()
+		{
+			
+		}
+}
